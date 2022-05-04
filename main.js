9e2cacc8a7442abfc77671431bd259c521d9c4cd
caathyn <caathynguyen@gmail.com>
2022-05-04 13:24:36 -0700

    setup for chrome extension

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
-const contents = document.getElementById('contents');
-contents.parentNode.removeChild(contents);
+// const contents = document.getElementById('contents');
+// contents.parentNode.removeChild(contents);
+const contents = $('#contents');
+const contentsParent = contents.parent();
+contents.remove();
