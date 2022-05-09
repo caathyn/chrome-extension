75d4a1180d96eb0c449bd95696a829a00286fd19
caathyn <caathynguyen@gmail.com>
2022-05-09 14:31:28 -0700

    setup for chrome extension

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 // const contents = document.getElementById('contents');
 // contents.parentNode.removeChild(contents);
+$('#chips-wrapper').remove();
 const contents = $('#contents');
 const contentsParent = contents.parent();
 contents.remove();
 
 const url = "https://media.wired.com/photos/5e3246cd56bcac00087f0a1e/1:1/w_1329,h_1329,c_limit/Culture-Success-Meme-Kid.jpg";
-contentsParent.prepend('<img src="https://media.wired.com/photos/5e3246cd56bcac00087f0a1e/1:1/w_1329,h_1329,c_limit/Culture-Success-Meme-Kid.jpg" />')
+const image = $(`<img src=${url} />`)
+contentsParent.prepend(image)
+
+const message = $('<p>Get back to achieving your coding dreams</p>')
+message.addClass('beautText')
+contentsParent.prepend(message)
