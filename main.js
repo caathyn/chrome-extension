878efb809968c0cca4178a0b5859f6097214740d
caathyn <caathynguyen@gmail.com>
2022-05-09 22:24:14 -0700

    building chrome extension project

--- main.js
+++ main.js
@@ -5,23 +5,24 @@ const contents = $('#contents');
 const contentsParent = contents.parent();
 contents.remove();
 
-const url = "https://media.wired.com/photos/5e3246cd56bcac00087f0a1e/1:1/w_1329,h_1329,c_limit/Culture-Success-Meme-Kid.jpg";
-const image = $(`<img src=${url} />`)
-contentsParent.prepend(image)
-
-const message = $('<p>Get back to achieving your coding dreams</p>')
-message.addClass('beautText')
-contentsParent.prepend(message)
-
-$('div').on('click', function() {
-  alert('A div was clicked!');
-});
 
 $.ajax({
   method: 'GET',
   url: 'https://unsplash.it/list',
   success: function(result) {
-    console.log("result -->", result)
+    const randomNum = Math.floor(Math.random() * result.length);
+
+    const url = "https://unsplash.it/1200/800?image=" + result[randomNum].id;
+    const image = $(`<img src=${url} />`)
+    contentsParent.prepend(image)
+    
+    const message = $('<p>Get back to achieving your coding dreams</p>')
+    message.addClass('beautText')
+    contentsParent.prepend(message)
+    
+    $('div').on('click', function() {
+      alert('A div was clicked!');
+    });
   },
   error: function(err) {
     console.log("error -->", err)
